8070af68f9ef6157e791a5b64f066b8975d7ab62
giannyw <andresfelipelealculma34@gmail.com>
2024-02-06 07:23:48 -0500

    inicio del proyecto

---
+++ hola.py
@@ -0,0 +1,13 @@
+
+
+Edad = int (input("Por favor ingresar su edad"))
+
+if Edad< 4:
+    Ingrero = 0
+elif Edad <=18: 
+    Ingreso = 5000
+else:
+    Ingreso = 10000
+
+print("El ingreso de la entrada es:", Ingreso)
+
